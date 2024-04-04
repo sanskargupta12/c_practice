3f7fb9c7c182ae911956f8612156f2621d7b6300
Sanskar Gupta <121814254+sanskargupta12@users.noreply.github.com>
2024-04-04 20:49:40 +0530

    Create factorial_recurssion.c

---
+++ factorial_recurssion.c
@@ -0,0 +1,20 @@
+// C program to find factorial of given number
+#include <stdio.h>
+
+// Function to find factorial of given number
+unsigned int factorial(unsigned int n)
+{
+	if (n == 1) {
+	return 1;
+	}
+
+	return n * factorial(n - 1);
+}
+
+// Driver code
+int main()
+{
+	int num = 5;
+	printf("Factorial of %d is %d", num, factorial(num));
+	return 0;
+}
